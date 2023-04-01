1c6ee16cdf0ef392bc3b4d4c25245ca8a654b9cc
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2023-04-01 17:06:49 -0300

    Exercício 2 (concluído)

---
+++ algoritmos-logicaProgramacao/listaExercicios0404/exe1.js
@@ -0,0 +1,8 @@
+//algoritmo que lê 2 números inteiros (base e expoente) e calcule a potência
+
+var num1 = 5
+var num2 = 2
+
+var potencia = num1
+
+// 5 p 2 = 5 . 5 = 25
---
+++ algoritmos-logicaProgramacao/listaExercicios0404/exe2.js
@@ -0,0 +1,97 @@
+//CABEÇALHO
+	//descrição: algoritmo que lê 3 números e aponta qual deles é o maior
+	//autor:     Gustavo Silva Souza
+	//data:      01/04/2023
+
+var num1 = 3
+var num2 = 3
+var num3 = 3
+
+/*
+	num1 num2 num3
+	num1 num3 num2
+
+	num2 num1 num3
+	num2 num3 num1
+
+	num3 num1 num2
+	num3 num2 num1
+	
+	num1 num2 | > num3
+	num1 num2 | < num3
+
+	num1 num3 | > num2
+	num1 num3 | < num2
+
+	num2 num3 | > num1
+	num2 num3 | < num1
+
+	num1 == num2 == num3
+*/
+
+if(num1 > num2 && num1 > num3 && num2 > num3){
+	console.log("O número " + num1 + " é o maior")
+}
+else{
+	if(num1 > num2 && num1 > num3 && num2 < num3){
+		console.log("O número " + num1 + " é o maior")
+	}
+	else{
+		if(num2 > num1 && num2 > num3 && num1 > num3){
+			console.log("O número " + num2 + " é o maior")
+		}
+		else{
+			if(num2 > num1 && num2 > num3 && num1 < num3){
+				console.log("O número " + num2 + " é o maior")
+			}
+			else{
+				if(num3 > num1 && num3 > num2 && num1 > num2){
+					console.log("O número " + num3 + " é o maior")
+				}
+				else{
+					if(num3 > num1 && num3 > num2 && num1 < num2){
+						console.log("O número " + num3 + " é o maior")
+					}
+					else{
+						if(num1 == num2 && num1 > num3){
+							console.log("Há dois números iguais (" + num1 + ") e esse valor é maior que o outro (" + num3 + ")")
+						}
+						else{
+							if(num1 == num2 && num1 < num3){
+								console.log("Há dois números iguais (" + num1 + ") e esse valor é menor que o outro (" + num3 + ")")
+							}
+							else{
+								if(num1 == num3 && num1 > num2){
+									console.log("Há dois números iguais (" + num1 + ") e esse valor é maior que o outro (" + num2 + ")")
+								}
+								else{
+									if(num1 == num3 && num1 < num2){
+										console.log("Há dois números iguais (" + num1 + ") e esse valor é menor que o outro (" + num2 + ")")
+									}
+									else{
+										if(num2 == num3 && num2 > num1){
+											console.log("Há dois números iguais (" + num2 + ") e esse valor é maior que o outro (" + num1 + ")")
+										}
+										else{
+											if(num2 == num3 && num2 < num1){
+												console.log("Há dois números iguais (" + num2 + ") e esse valor é menor que o outro (" + num1 + ")")
+											}
+											else{
+												if(num1 == num2 && num1 == num3){
+													console.log("Nenhum número é maior que o outro. Todos são iguais (" + num1 + ")")
+												}
+												else{
+													console.log("Ocorreu algum erro durante a execução da aplicação")
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
