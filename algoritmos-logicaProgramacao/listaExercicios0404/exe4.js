a1816e39f07abcf0eacceaf7cce508d1d6f3125c
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2023-04-01 18:47:58 -0300

    Finalização do exercício 4

---
+++ algoritmos-logicaProgramacao/listaExercicios0404/exe4.js
@@ -0,0 +1,18 @@
+//CABEÇALHO
+	//descrição: programa que cria duas variáveis e troca o valor delas
+	//autor: Gustavo Silva Souza
+	//data: 01/04/2023
+
+//VARIÁVEIS
+	//float num1, num2, num3
+
+//INÍCIO
+	var num1 = 10
+	var num2 = 35
+
+	var num3 = num1
+	var num1 = num2
+	var num2 = num3
+
+	console.log("Valor de num1: " + num1)
+	console.log("Valor de num2: " + num2)
