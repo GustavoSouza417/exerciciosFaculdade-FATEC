a1e076a6534b7304fe9d57769e281045f91de9c0
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-02-28 10:49:43 -0300

    Add files via upload

---
+++ trocaDeValoresEntreVariaveis.cpp
@@ -0,0 +1,59 @@
+/*
+	Author: Gustavo Silva Souza
+	Date: 28/02/24 09:50
+	Description: programa que lê duas variáveis e troca o valor delas entre elas
+*/
+
+
+
+//bibliotecas
+#include <stdio.h>
+#include <locale.h>
+
+
+
+//prototipação
+int lerNum();
+void trocaValores(int *, int *);
+void exibirTroca(int, int);
+
+
+
+//função principal
+main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int a, b;
+	a = b = 0;
+	
+	a = lerNum();
+	b = lerNum();
+	
+	trocaValores(&a, &b);
+	exibirTroca(a, b);
+}
+
+
+
+//funções
+int lerNum()
+{
+	int numero = 0;
+	printf("Digite um número: ");
+	scanf("%i", &numero);
+	return numero;
+}
+
+void trocaValores(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void exibirTroca(int a, int b)
+{
+	printf("A: %i\n", a);
+	printf("B: %i", b);
+}
